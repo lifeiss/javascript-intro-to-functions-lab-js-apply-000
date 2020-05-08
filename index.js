d4cc9672885ed8369501fccd5a93f44f221c6490
Jacqueline Nyiraturatsinze <nturatsinzy@yahoo.fr>
2020-05-08 17:35:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function whisper(string) {return string .toLowerCase()}
 function logShout(string) {console.log(string .toUpperCase());}
 function logWhisper(string) {console.log(string .toLowerCase());}
 function sayHiToGrandma(){
-  var 
   var cantUnswer = "I can't hear you!";
   var yesUnswer = "YES INDEED!";
   var lovUnswer = "I love you, too.";
